b7f46d603e22101205f8ca2c691dd9fbe5af7d16
Christian Sassi <christian.sassii@gmail.com>
2024-08-07 09:00:56 +0200

    -

--- discovery_piscine/cell01/ex00/name.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-def main():
-    first_name = "Christian"
-    last_name = "Sassi"
-    print(first_name, last_name)
-
-if __name__ == "__main__":
-    main()
--- discovery_piscine/cell04/ex00/upcase_it.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-def main():
-    get = input("Give me a word: ")
-    new = get.upper()
-    print(new)
-
-if __name__ == "__main__":
-    main()
